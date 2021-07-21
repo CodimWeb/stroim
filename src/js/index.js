1953c6300c3e769e4fab90b3c8ffc0166da06538
Valeriy <codimweb@gmail.com>
2021-07-21 22:48:16 +0300

    start

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// import $ from 'jquery';
+
+//BS4 components
+// import bootstrap from 'bootstrap';
+
+//styles
+import '../scss/style.scss';
+
+
+
+
+$(document).ready(function(){
+    
+})
+
