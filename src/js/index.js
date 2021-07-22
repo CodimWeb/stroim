d92ea047bf1b2b87bfc4abceb4da754150c9ddb2
Valeriy <codimweb@gmail.com>
2021-07-22 22:43:51 +0300

    start select 2

--- src/js/index.js
+++ src/js/index.js
@@ -8,10 +8,27 @@
 //styles
 import '../scss/style.scss';
 
+import select2 from 'select2';
+
+console.log(select2);
 
 
 
 $(document).ready(function(){
-    
+    $('.materil-group__input').on('focus', function(){
+        $(this).closest('.materil-group').addClass('active');
+    })
+
+    $('.materil-group__input').on('blur', function(e){
+        if(e.target.value == '') {
+            $(this).closest('.materil-group').removeClass('active');
+        }
+    })
+
+    $('.test').select2({
+        width: '100%',
+        selectionCssClass: 'test',
+        dropdownCssClass: 'test-dropdown'
+    });
 })
 
