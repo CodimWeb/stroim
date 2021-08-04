d6b3e6b5cffeae3cb8948bb537ab0473763feb29
Виталий Шлепаков <shlepakov58@gmail.com>
2021-08-04 22:12:23 +0200

    закончил страницы сервисы и компания из чёрного списка с блоком комментариев

--- webpack.prod.js
+++ webpack.prod.js
@@ -106,6 +106,22 @@ module.exports = {
           template: './src/password-recovery-3.html'
         }),
         new HtmlWebpackPlugin({
+            filename: 'blacklist.html',
+            template: './src/blacklist.html'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'blacklist-company.html',
+            template: './src/blacklist-company.html'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'add-to-blacklist.html',
+            template: './src/add-to-blacklist.html'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'services.html',
+            template: './src/services.html'
+        }),
+        new HtmlWebpackPlugin({
           filename: 'ui.html',
           template: './src/ui.html'
         }),
